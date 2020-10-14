13e43c5e0c853d84f611f87c4fdd24afeb51a7be
Christian Mills <millsy4@hotmail.com>
2020-10-14 01:29:54 -0500

    got storage functionality working now

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-var hour = moment().hour();
-console.log(hour)
-var i = 11;
-var test = $(".number-" + 12).attr('value');
-console.log(test);
+// var hour = moment().hour();
+var hour = 12;
+// console.log(hour)
+// var i = 11;
+// var test = $(".number-" + 12).attr('value');
+// console.log(test);
 backgroundColors();
 function backgroundColors() {
-  for (var i = 0; i < 24; i++) {
-    if (i < test) {
+  for (var i = 9; i <= 17; i++) {
+    if (i < hour) {
       $(".number-" + i).addClass('past');
+    }
+    else if (i == hour) {
+      $(".number-" + i).addClass('present');
+    }
+    else if (i > hour) {
+      $(".number-" + i).addClass('future');
     }
   }
 }
+
+$('.saveBtn').on('click', function() {
+  for (var i = 9; i <= 17; i++) {
+    if ($('ta' + i) != null) {
+      var test = $('.ta-' + i).val();
+      console.log(test);
+      localStorage.setItem(i, test);
+    }
+  }
+})
