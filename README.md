74c259e30cd19003a27a6de0a9b6c4a1c9aaa267
Christian Mills <millsy4@hotmail.com>
2020-10-15 23:13:15 -0500

    added comments and deleted pseudo.txt

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# 05_work_day_scheduler
+# [05_work_day_scheduler](https://millsy4.github.io/05_work_day_scheduler/)
+
+Tools I used to complete this include:
+
+- [Javascript](https://www.javascript.com/)
+
+- [JQuery](https://jquery.com/)
+
+- [Moment.JS](https://momentjs.com/)
+
+![Picture of Main Page](./images/website_screenshot.png)
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Create variables to hold the information that we get from momentjs
 var hour = moment().hour();
-var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 var dayOfWeek = moment().isoWeekday();
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var month = moment().month();
 var dateOfWeek = moment().date();
 
+// Create a variable arrays to hold the months and the days of the week
+var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// Create a function that controls the background colors of the columns depending on time
 function backgroundColors() {
   for (var i = 9; i <= 17; i++) {
     if (i < hour) {
@@ -19,6 +23,7 @@ function backgroundColors() {
   }
 }
 
+// Add a click function to the save button that will take the information in the textarea and save it to local storage
 $('.saveBtn').on('click', function() {
   for (var i = 9; i <= 17; i++) {
     if ($('ta' + i) !== null) {
@@ -28,6 +33,7 @@ $('.saveBtn').on('click', function() {
   }
 })
 
+// Create a function that will get the values from the local storage and display them on page reloads
 function getLocalStor() {
   for (var i = 9; i <= 17; i++) {
     var getStor = localStorage.getItem(i);
@@ -36,11 +42,13 @@ function getLocalStor() {
   }
 }
 
+// Create a function that sets the day of the week depending on the variables from momentjs
 function setDay() {
   var day = daysOfWeek[(dayOfWeek - 1)]
   $('#currentDay').text(day + ', ' + months[month] + ' ' + dateOfWeek);
 }
 
+// Initialize all starting functions
 getLocalStor();
 setDay();
 backgroundColors();
